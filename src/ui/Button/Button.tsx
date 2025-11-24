39b4c5d77e513e229a4e4d0ffb5aaa7e40de1fcf
VeligorMan <spaunroom@gmail.com>
2025-11-24 15:51:29 +0300

    add responsive design, improve form and transaction cards layout

---
+++ src/ui/Button/Button.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./Button.module.scss";
+
+export default function Button({
+  children,
+  ...props
+}: React.ButtonHTMLAttributes<HTMLButtonElement>) {
+  return (
+    <button className={styles.button} {...props}>
+      {children}
+    </button>
+  );
+}
