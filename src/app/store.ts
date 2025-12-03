1656bc9bb4465c7e33f9b4d5fdfdf1de9e35bb0a
VeligorMan <spaunroom@gmail.com>
2025-12-03 18:16:06 +0300

    add redux persist for saving transactions

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,25 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import transactionsReducer from "../features/transactions/transactionsSlice";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
-export const store = configureStore({
-  reducer: { transactions: transactionsReducer },
+const rootReducer = combineReducers({
+  transactions: transactionsReducer,
 });
 
-// Типы стора
+// Конфиг Persist
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+});
+
+export const persistor = persistStore(store);
+
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
