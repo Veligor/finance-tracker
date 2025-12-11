ce4ce9bdd23cec449e6f03b62eb089bcbc2c9149
VeligorMan <spaunroom@gmail.com>
2025-12-11 17:07:45 +0300

    add ability to delete transactions on operations page

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,3 @@
-// import React from "react";
-// import MainLayout from "./layouts/MainLayout";
-// import Home from "./pages/Home/Home";
-// import Stats from "./pages/Stats/Stats";
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-// export default function App() {
-//   return (
-//     <MainLayout>
-//       <Home />
-//       <Route path="/stats" element={<Stats />} />
-//     </MainLayout>
-//   );
-// }
 import React from "react";
 import MainLayout from "./layouts/MainLayout";
 import Home from "./pages/Home/Home";
